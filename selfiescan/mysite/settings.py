e2fef9b8053fc47114d6d9802997bc97dd6237d5
ashwin rangani <ashwyn.rangani@gmail.com>
2025-02-28 00:53:32 +0530

    social login and signup teplates

--- selfiescan/mysite/settings.py
+++ selfiescan/mysite/settings.py
@@ -30,12 +30,13 @@ SECRET_KEY = 'django-insecure-bl75(lby!wu6q)dehh1h4j=lj4wsv)&bititpjuc8b22bjv@ov
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['localhost', '127.0.0.1', '2692-27-59-84-43.ngrok-free.app','cb7f-27-59-94-28.ngrok-free.app']
-# CSRF_TRUSTED_ORIGINS = [
-#     'https://2692-27-59-84-43.ngrok-free.app',
-#     'https://cb7f-27-59-94-28.ngrok-free.app'
-# ]
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '2692-27-59-84-43.ngrok-free.app','5029-27-59-85-65.ngrok-free.app']
+CSRF_TRUSTED_ORIGINS = [
+    'https://2692-27-59-84-43.ngrok-free.app',
+    'https://cb7f-27-59-94-28.ngrok-free.app',
+    'https://5029-27-59-85-65.ngrok-free.app',
+]
 
 
 # Application definition
@@ -80,11 +81,12 @@ SOCIALACCOUNT_PROVIDERS = {
         # (``socialaccount`` app) containing the required client
         # credentials, or list them here:
         'APP': {
-            'client_id': '123',
-            'secret': '456',
+            'client_id': '493493127120-1hjgghuljq7lrfrc77vuonn3jq3u1560.apps.googleusercontent.com',
+            'secret': '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',
             'key': ''
         }
-    }
+    },
+    
 }
 
 ROOT_URLCONF = 'mysite.urls'
--- selfiescan/static/js/base.js
+++ selfiescan/static/js/base.js
@@ -1,28 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const notyf = new Notyf({
-        duration: 5000,
-        position: { x: 'right', y: 'bottom' },
+  const notyf = new Notyf({
+    duration: 5000,
+    position: { x: "right", y: "bottom" },
+    types: [
+      {
+        type: "info",
+        background: "blue",
+        icon: false,
+      },
+    ],
+  });
 
-    });
+  const messagesScript = document.getElementById("messages-data");
 
-    const messagesScript = document.getElementById("messages-data");
+  if (messagesScript) {
+    try {
+      const messages = JSON.parse(messagesScript.textContent);
 
-    if (messagesScript) {
-        try {
-            const messages = JSON.parse(messagesScript.textContent);
-
-            messages.forEach(msg => {
-                if (msg.tags.includes("success")) {
-                    notyf.success(msg.message);
-                } else if (msg.tags.includes("error") || msg.tags.includes("danger")) {
-                    notyf.error(msg.message);
-                } else {
-                    notyf.open({ type: 'info', message: msg.message });
-                }
-            });
-
-        } catch (error) {
-            console.error("Error parsing messages:", error);
+      messages.forEach((msg) => {
+        if (msg.tags.includes("success")) {
+          notyf.success(msg.message);
+        } else if (msg.tags.includes("error") || msg.tags.includes("danger")) {
+          notyf.error(msg.message);
+        } else {
+          notyf.success({ type: "info", message: msg.message });
         }
+      });
+    } catch (error) {
+      console.error("Error parsing messages:", error);
     }
+  }
 });
